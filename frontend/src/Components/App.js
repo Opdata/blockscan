bee2971ff181dfdad24d8cc6f4e2cb5051635701
Opdata <pb03641@gmail.com>
2020-03-06 20:02:09 +0900

    [feature] 기본적인 Body 스타일링
    - 차후 App.js 의 Body 스타일링을 BodyStyle 로 분리하여 적용할 예정

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -6,12 +6,19 @@ import Header from './Containers/Header/ReturnHeader';
 
 const Wrapper = styled.div``;
 
+const Body = styled.div`
+  height: 100%;
+  background-color: darkkhaki;
+  padding: 0px 69px 0px 69px;
+`;
+
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
       <Wrapper>
         <Header />
+        <Body></Body>
       </Wrapper>
     </ThemeProvider>
   );
