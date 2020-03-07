14c13c6d7cd9f0c9c2514aae4097d97cc31a1a14
Opdata <pb03641@gmail.com>
2020-03-07 17:30:37 +0900

    [feature] RenderTitle 에 노션 링크 추가

--- frontend/src/Components/Containers/Content/Title/RenderSponsoredTitle.js
+++ frontend/src/Components/Containers/Content/Title/RenderSponsoredTitle.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.div`
-  width: 100%;
   height: 100%;
   display: flex;
   padding: 20px 0px 12px 0px;
@@ -10,16 +9,26 @@ const Title = styled.div`
   border: solid 1px;
 `;
 
-const TitleText = styled.h3`
+const TitleText = styled.ins`
   color: #6c757e;
   font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   font-size: 0.8125rem;
+  text-decoration: none;
+`;
+
+const Link = styled.a`
+  color: #3498e1;
 `;
 
 const Sponsored = () => {
   return (
     <Title>
-      <TitleText>Sponsored: IGMM</TitleText>
+      <TitleText>
+        Sponsored:
+        <Link href={'https://www.notion.so/IGMM-BLOCK-EXPLORER-88a81bf42fbc40b2b614724562dde11b'}>
+          Visit IGMM Notion
+        </Link>
+      </TitleText>
     </Title>
   );
 };
