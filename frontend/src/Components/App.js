d14a81bf39b9e23fb2556f7d91b6b931fd13fe6f
Opdata <pb03641@gmail.com>
2020-03-10 23:37:55 +0900

    [feature] 기본적인 EtherInfo 틀 렌더링 - 스타일링 미완성
    - App.js EtherInfo 컴포넌트 추가
    - Block, Difficulty, Market, Price 아이콘(SVG) 추가, Chart 이미지 추가
    - RenderEtherInfo.js 기본적인 EtherInfo 렌더링 - 스타일링 미완성
    - ReturnEtherInfo.js RenderEtherInfo 컴포넌트를 받아오는 중간역할 컴포넌트

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -24,7 +24,7 @@ const App = () => {
         <Body>
           <SponsoredTitle />
           <SearchBar />
-          {/* <EtherInfo /> */}
+          <EtherInfo />
         </Body>
       </Wrapper>
     </ThemeProvider>
