fa683942eb98acf4f8f83d0150ce4d531ae450d0
Opdata <pb03641@gmail.com>
2020-03-16 08:10:25 +0900

    [feature] 기본적인 SearchBar 스타일링
    - RenderSearchBar.js SearchBar Title 컴포넌트 추가, QuickLinks 컴포넌트 추가, Form 으로 묶지 않은 상태로 Select, Input, Button 추가
    - ReturnSearchBar.js RenderSearchBar 컴포넌트를 받아오는 중간역할 컴포넌트
    - FilterData.js StyledSelect 필터 옵션 데이터

---
+++ frontend/src/Components/Containers/Content/SearchBar/RenderSearchBar.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Select from 'react-select';
+import ReactSearchBox from 'react-search-box';
+import FilterData from './FilterData';
+
+const Search = styled.div`
+  height: 109px;
+  padding: 12px 20px 20px 20px;
+  background-image: linear-gradient(to right, #1e83e5, #273ea7);
+`;
+
+const SearchDiv = styled.div`
+  margin: 0px 0px 20px 0px;
+`;
+
+const TitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  border: solid 1px;
+  margin-bottom: 8px;
+`;
+
+const SearchBarTitle = styled.h2``;
+
+const QuickLinks = styled.h3`
+  font-size: 0.8125rem;
+  cursor: pointer;
+  border-bottom: 1px dashed #97a4af;
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  border: solid 1px;
+`;
+
+const StyledSelect = styled(Select)`
+  width: 116px;
+  font-size: 0.8125rem;
+`;
+
+const StyledSearchBox = styled.input`
+  width: 100%;
+`;
+
+const StyledButton = styled.button`
+  width: 75px;
+`;
+
+const RenderSearchBar = () => {
+  const [select, setSelect] = useState(FilterData[0]);
+
+  const handleChange = select => {
+    setSelect(select);
+  };
+
+  return (
+    <Search>
+      <SearchDiv>
+        <TitleBox>
+          <SearchBarTitle>Ethereum BlockChain Explorer</SearchBarTitle>
+          <QuickLinks>Quick links: ERC-20 Quick links</QuickLinks>
+        </TitleBox>
+        <SearchBox>
+          <StyledSelect
+            defaultValue={select}
+            value={select}
+            onChange={handleChange}
+            options={FilterData}
+          />
+          <StyledSearchBox />
+          <StyledButton>Search</StyledButton>
+        </SearchBox>
+      </SearchDiv>
+    </Search>
+  );
+};
+
+export default RenderSearchBar;
