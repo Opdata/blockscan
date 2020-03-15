36b19f1cb816512b80d37da083842d52c1dbb45f
Opdata <pb03641@gmail.com>
2020-03-16 08:08:19 +0900

    [refactor] SearchBar 컴포넌트에 맞게 App.js 최적화

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../Styles/GlobalStyle';
 import Theme from '../Styles/Theme';
+import SearchBar from './Containers/Content/SearchBar/ReturnSearchBar';
 
-const Wrapper = styled.div``;
+const Body = styled.div`
+  background-color: #f8f9fa;
+`;
+
+//27인치 확인시 66가 width 동일
+
+const Main = styled.div`
+  height: 100%;
+  width: 66%;
+  margin: 0 auto;
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
-      <Wrapper></Wrapper>
+      <Body>
+        <Main>
+          <SearchBar />
+        </Main>
+      </Body>
     </ThemeProvider>
   );
 };
