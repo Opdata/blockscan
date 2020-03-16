8f24eda42119e60aa3986f21c61607103bc4eb9e
Opdata <pb03641@gmail.com>
2020-03-16 10:51:46 +0900

    [feature] SponsoredTitle 컴포넌트 추가

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../Styles/GlobalStyle';
 import Theme from '../Styles/Theme';
+import Header from './Containers/Header/ReturnHeader';
+import SponsoredTitle from './Containers/Content/Title/ReturnSponsoredTitle';
 
-const Wrapper = styled.div``;
+const Body = styled.div`
+  background-color: #f8f9fa;
+`;
+
+const Main = styled.div`
+  height: 100%;
+  width: 66%;
+  margin: 0 auto;
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
-      <Wrapper></Wrapper>
+      <Body>
+        <Header />
+        <Main>
+          <SponsoredTitle />
+        </Main>
+      </Body>
     </ThemeProvider>
   );
 };
